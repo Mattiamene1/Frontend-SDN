62fb022f40a7d0c7123e134b9579d6f831e51093
GoldenRose01 <116004785+GoldenRose01@users.noreply.github.com>
2023-10-14 11:21:31 +0200

    Fix--

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from './node_modules/vue/index.js';
 const app = createApp(App);
 
 createApp({
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const axios = require('axios').default;
+
+// Imposta la porta su 3000
+const PORT = process.env.PORT || 5500;
+
+// Definisci una directory statica per servire il file HTML
+app.use(express.static(path.join(__dirname)));
+
+// Gestisci richieste GET sulla radice (index.html)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Aggiungi una route per ottenere dati dal processo backend
+app.get('/getHostsData', async (req, res) => {
+    try {
+      const response = await axios.get('http://localhost:3000/hosts');
+      const data = response.data;
+      res.json({ hostsData: data });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Errore nel recupero dei dati' });
+    }
+  });
+
+// Avvia il server sulla porta 5500
+app.listen(PORT, () => {
+  console.log(`Server avviato su http://localhost:${PORT}`);
+});
