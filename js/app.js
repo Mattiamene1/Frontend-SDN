aa2a7bb4ecda7cfe307aae8f060dc48755aef326
GoldenRose01 <116004785+GoldenRose01@users.noreply.github.com>
2024-05-23 16:30:36 +0200

    Fixx?

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const { createApp } = Vue;
+const { createApp } = Vue
 
 createApp({
   data() {
@@ -17,13 +17,13 @@ createApp({
         // configuration for links
         linkConfig: {
           linkType: "straight",
-          color: "model.color",
+          color: "model.color"
         },
         // if true, the nodes' icons are shown, a dot is shown instead
         showIcon: true,
         //enableSmartNode: true,
         //autoLayout: true,
-        dataProcessor: "force",
+        dataProcessor: "force"
       },
       auto_update: false,
       auto_update_interval: null,
@@ -34,6 +34,7 @@ createApp({
       host: {},
       host_id: null,
       host_detail: false,
+
       // Translation related data
       title: '',
       autoUpdateButtonText: '',
@@ -59,96 +60,96 @@ createApp({
       closeHostDetailsButton: '',
       connectedHostPortTitle: '',
       lang: 'it'
-    };
+    }
   },
   watch: {
     flow_table_hidden: function (newVal, oldVal) {
-      this.topology.adaptToContainer();
-    },
+      this.topology.adaptToContainer()
+    }
   },
   methods: {
     change_auto_update: function () {
-      let vm = this;
+      let vm = this
       if (this.auto_update) {
-        clearInterval(this.auto_update_interval);
-        this.auto_update_interval = null;
-        this.auto_update = false;
+        clearInterval(this.auto_update_interval)
+        this.auto_update_interval = null
+        this.auto_update = false
       } else {
         this.auto_update_interval = setInterval(async function () {
-          reset_topology();
-          hosts = await get_hosts();
-          switches = await get_switches();
-          links = await get_links();
+          reset_topology()
+          hosts = await get_hosts()
+          switches = await get_switches()
+          links = await get_links()
 
           if (!vm.topology) {
-            vm.init_topology();
+            vm.init_topology()
           }
 
-          vm.topology.setData(build_topology());
+          vm.topology.setData(build_topology())
           if (vm.switch_detail) {
-            vm.show_switch(vm.switch_id);
+            vm.show_switch(vm.switch_id)
           }
           if (vm.host_detail) {
-            vm.show_host(vm.host_id);
+            vm.show_host(vm.host_id)
           }
-        }, 10 * 1000); // 10 seconds
-        this.auto_update = true;
+        }, 5 * 1000); // 5 seconds
+        this.auto_update = true
       }
     },
     vertical_layout: function () {
-      var layout = this.topology.getLayout("hierarchicalLayout");
-      layout.direction("vertical");
-      layout.sortOrder(["switch", "host"]);
+      var layout = this.topology.getLayout('hierarchicalLayout');
+      layout.direction('vertical');
+      layout.sortOrder(['switch', 'host']);
       layout.levelBy(function (node, model) {
-        return model.get("device_type");
+        return model.get('device_type');
       });
-      this.topology.activateLayout("hierarchicalLayout");
+      this.topology.activateLayout('hierarchicalLayout');
     },
     horizontal_layout: function () {
-      var layout = this.topology.getLayout("hierarchicalLayout");
-      layout.direction("horizontal");
-      layout.sortOrder(["switch", "host"]);
+      var layout = this.topology.getLayout('hierarchicalLayout');
+      layout.direction('horizontal');
+      layout.sortOrder(['switch', 'host']);
       layout.levelBy(function (node, model) {
-        return model.get("device_type");
+        return model.get('device_type');
       });
-      this.topology.activateLayout("hierarchicalLayout");
+      this.topology.activateLayout('hierarchicalLayout');
     },
     show_flow_table: async function (id) {
-      let res = await get_flow_table(id);
-      this.flow_table = res[id];
+      let res = await get_flow_table(id)
+      this.flow_table = res[id]
     },
     show_switch: function (id) {
-      this.switch_id = id;
+      this.switch_id = id
       for (switch_ of switches) {
         if (id === parseInt(switch_.dpid)) {
-          this.switch_ = switch_;
-          break;
+          this.switch_ = switch_
+          break
         }
       }
-      this.show_flow_table(id);
-      this.switch_detail = true;
+      this.show_flow_table(id)
+      this.switch_detail = true
     },
     hide_switch: function () {
-      this.switch_detail = false;
+      this.switch_detail = false
     },
     show_host: function (id) {
-      this.host_id = id;
+      this.host_id = id
       for (host of hosts) {
         if (host.ipv4.includes(id) || host.ipv6.includes(id)) {
-          this.host = host;
-          break;
+          this.host = host
+          break
         }
       }
-      this.host_detail = true;
+      this.host_detail = true
     },
     hide_host: function () {
-      this.host_detail = false;
+      this.host_detail = false
     },
     init_topology: async function () {
-      hosts = await get_hosts();
-      switches = await get_switches();
-      links = await get_links();
-      let vm = this;
+      hosts = await get_hosts()
+      switches = await get_switches()
+      links = await get_links()
+      let vm = this
 
       // instantiate next app
       const app = new nx.ui.Application();
@@ -161,27 +162,27 @@ createApp({
 
       // bind the topology object to the app
 
-      this.topology.on("topologyGenerated", function () {
+      this.topology.on('topologyGenerated', function () {
         vm.topology.eachNode(function (callback, context) {
-          callback.on("clickNode", function () {
-            let nodeType = arguments[0].iconType();
-            if (nodeType == "switch") {
-              let id = arguments[0].label();
-              vm.show_switch(id);
-            } else if (nodeType == "host") {
-              let id = arguments[0].label();
-              vm.show_host(id[0]);
+          callback.on('clickNode', function () {
+            let nodeType = arguments[0].iconType()
+            if (nodeType == 'switch') {
+              let id = arguments[0].label()
+              vm.show_switch(id)
+            } else if (nodeType == 'host') {
+              let id = arguments[0].label()
+              vm.show_host(id[0])
             }
-          });
-        });
-      });
+          })
+        })
+      })
 
       this.topology.attach(app);
 
       // app must run inside a specific container. In our case this is the one with id="topology-container"
       app.container(document.getElementById("topology-container"));
 
-      const elements = document.getElementsByClassName("n-popupContainer");
+      const elements = document.getElementsByClassName('n-popupContainer');
       while (elements.length > 0) {
         elements[0].parentNode.removeChild(elements[0]);
       }
@@ -222,7 +223,7 @@ createApp({
     },
   },
   mounted: function () {
-    this.init_topology();
-    this.loadTranslation("it"); // Load default language
+    this.init_topology()
+    this.loadTranslation("it") // Load default language
   },
-}).mount("#app");
+}).mount("#app")
--- js/topology.js
+++ js/topology.js
@@ -1,52 +1,52 @@
-const BASEURL = "http://localhost:3000";
+const BASEURL = "http://localhost:3000"
 
-let hosts = [];
-let switches = [];
-let links = [];
-let id_map = {};
-let next_id = 0;
-let ports_switch = {};
+let hosts = []
+let switches = []
+let links = []
+let id_map = {}
+let next_id = 0
+let ports_switch = {}
 
 async function get_hosts() {
-  let res = await axios.get(BASEURL + "/hosts");
-  return res.data;
+  let res = await axios.get(BASEURL + "/hosts")
+  return res.data
 }
 async function get_switches() {
-  let res = await axios.get(BASEURL + "/switches");
-  return res.data;
+  let res = await axios.get(BASEURL + "/switches")
+  return res.data
 }
 async function get_links() {
-  let res = await axios.get(BASEURL + "/links");
-  return res.data;
+  let res = await axios.get(BASEURL + "/links")
+  return res.data
 }
 
 async function get_flow_table(id) {
-  let res = await axios.get(BASEURL + "/stats/flow/" + id);
-  return res.data;
+  let res = await axios.get(BASEURL + '/stats/flow/' + id)
+  return res.data
 }
 
 function generate_id_int(id_str) {
-  id_str = id_str.replace("s", 1);
+  id_str = id_str.replace('s', 1);
   return id_str;
 }
 
 function reset_topology() {
-  id_map = {};
-  next_id = 0;
-  ports_switch = {};
+  id_map = {}
+  next_id = 0
+  ports_switch = {}
 }
 
 function build_topology() {
-  let res = [];
-  let nodes = [];
-  let edges = [];
+  let res = []
+  let nodes = []
+  let edges = []
 
   if (!switches) {
-    switches = [];
+    switches = []
   }
   for (let [idx, switch_] of switches.entries()) {
-    let id = switch_.ports[0].name.split("-")[0] || switch_.dpid;
-    id_map[id] = next_id;
+    let id = switch_.ports[0].name.split('-')[0] || switch_.dpid
+    id_map[id] = next_id
     next_id++;
 
     nodes.push({
@@ -55,52 +55,52 @@ function build_topology() {
       //y: 100,
       name: parseInt(switch_.dpid),
       device_type: "switch",
-    });
+    })
 
     for (let [idx2, port] of switch_.ports.entries()) {
-      ports_switch[port.name] = id;
+      ports_switch[port.name] = id
     }
   }
 
   if (!hosts) {
-    hosts = [];
+    hosts = []
   }
   for (let [idx, host] of hosts.entries()) {
-    let ip = [host.ipv4[0] || host.ipv6[0]];
-    id_map[host.port.name] = next_id;
-    next_id++;
+    let ip = [host.ipv4[0] || host.ipv6[0]]
+    id_map[host.port.name] = next_id
+    next_id++
     nodes.push({
       id: host.port.name,
       //x: (400 + 200 * idx),
       //y: 200,
       name: ip,
-      device_type: "host",
-    });
+      device_type: "host"
+    })
     edges.push({
       source: id_map[host.port.name],
-      target: id_map[host.port.name.split("-")[0]],
-    });
+      target: id_map[host.port.name.split('-')[0]]
+    })
   }
 
   if (!links) {
-    links = [];
+    links = []
   }
   for (let [idx, link] of links.entries()) {
     if (!link.src || !link.dst) {
-      continue;
+      continue
     }
 
-    source = id_map[link.src.name] || id_map[ports_switch[link.src.name]];
-    target = id_map[link.dst.name] || id_map[ports_switch[link.dst.name]];
+    source = id_map[link.src.name] || id_map[ports_switch[link.src.name]]
+    target = id_map[link.dst.name] || id_map[ports_switch[link.dst.name]]
 
-    let present = false;
+    let present = false
     for (let edge of edges) {
       if (
         (edge.source == source && edge.target == target) ||
         (edge.source == target && edge.target == source)
       ) {
-        present = true;
-        break;
+        present = true
+        break
       }
     }
 
@@ -108,15 +108,15 @@ function build_topology() {
       edges.push({
         source: source,
         target: target,
-        color: "red",
-      });
+        color: 'red'
+      })
     }
   }
 
   res = {
     nodes,
-    links: edges,
+    links: edges
   };
 
-  return res;
+  return res
 }
